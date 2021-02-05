f0ba916a9017744cc701ed427bc057443fe6f23a
Ngô Ngọc Đức Huy <huyngo@disroot.org>
2021-02-05 13:52:46 +0700

    Add Phong's patch to fix segfault

--- pw1/chat_client.c
+++ pw1/chat_client.c
@@ -1,44 +1,36 @@
-
+#include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-    int so;
-    char s[100];
-    struct sockaddr_in ad;
+int main(int argc, char *argv[])
+{
+	/* TCP/IPv4 */
+	struct addrinfo hints = {
+		.ai_socktype = SOCK_STREAM,
+		.ai_family = AF_INET,
+	};
+	struct addrinfo *res;
 
-    socklen_t ad_length = sizeof(ad);
-    struct hostent *hep;
+	/* No error checking, sorry!  Use GDB if needed. */
+	getaddrinfo(NULL, "12345", &hints, &res);
+	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	connect(sockfd, res->ai_addr, res->ai_addrlen);
 
-    // create socket
-    int serv = socket(AF_INET, SOCK_STREAM, 0);
+	char buffer[100];
+	while (1) {
+		printf("client>");
+		scanf("%s", buffer);
+		send(sockfd, buffer, strlen(buffer), 0);
 
-    // init address
-    hep = gethostbyname(argv[1]);
-    memset(&ad, 0, sizeof(ad));
-    ad.sin_family = AF_INET;
-    ad.sin_addr = *(struct in_addr *)hep->h_addr_list[0];
-    ad.sin_port = htons(12345);
+		recv(sockfd, buffer, sizeof(buffer), 0);
+		printf("server says: %s\n", buffer);
+	}
 
-    // connect to server
-    connect(serv, (struct sockaddr *)&ad, ad_length);
-
-    while (1) {
-        // after connected, it's client turn to chat
-
-        // send some data to server
-        printf("client>");
-        scanf("%s", s);
-        write(serv, s, strlen(s) + 1);
-
-        // then it's server turn
-        read(serv, s, sizeof(s));
-
-        printf("server says: %s\n", s);
-    }
+	freeaddrinfo(res);
+	return 0;
 }
-
