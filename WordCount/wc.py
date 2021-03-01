fa97b8f243c87fd09c083d3e4adc293bb67f6c91
Ngô Ngọc Đức Huy <huyngo@disroot.org>
2021-03-01 16:18:52 +0700

    Implement word count

---
+++ WordCount/wc.py
@@ -0,0 +1,21 @@
+from mrjob.job import MRJob
+import re
+
+word_regex = re.compile(r"[\w']+")
+
+
+class WordCount(MRJob):
+
+    def mapper(self, _, line):
+        for word in word_regex.findall(line):
+            yield (word.lower(), 1)
+
+    def combiner(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+
+if __name__ == '__main__':
+     WordCount.run()
