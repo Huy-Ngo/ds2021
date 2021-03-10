30b5f16e4b1ebfefef574abef54ae7f3c3b9ef94
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2021-03-10 15:41:22 +0700

    Clean the unnecessary file

--- mpi/mpi.py
+++ mpi/mpi.py
@@ -5,11 +5,10 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if (rank == 0):
-    f = open("logo-usth-pa3-01.png", "rb")
+    f = open("../logo-usth-pa3-01.png", "rb")
     data = f.read()
-    comm.send(data, dest=1, tag=42)
+    comm.send(data, dest=1)
 elif (rank == 1):
-    data = comm.recv(source=0, tag=42)
+    data = comm.recv(source=0)
     f = open("logo.png", "wb")
     f.write(data)
-    print(data)
